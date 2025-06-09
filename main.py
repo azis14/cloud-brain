a851b3971cf056c6063ca94a457ca2204b53352d
muhammadazishusein <muhammadazishusein@gmail.com>
2025-06-09 21:10:52 +0700

    chore: remove versions on reqs

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from typing import List, Dict, Any, Optional
 import os
 from dotenv import load_dotenv
 from notion_client import Client
